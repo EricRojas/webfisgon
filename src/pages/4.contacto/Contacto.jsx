d6951b9ca911b54f1de171ee40cd36d017600991
EricRojas <ericrojasf@hotmail.com>
2025-05-30 19:42:40 -0500

    Correccion background Formulario Contacto3

--- src/pages/4.contacto/Contacto.jsx
+++ src/pages/4.contacto/Contacto.jsx
@@ -350,7 +350,7 @@ const ContactForm = () => {
         </button>
         <button
           type="submit"
-          className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-gray-900 shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-gray-900 dark:text-gray-200 shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
         >
           Enviar
         </button>
